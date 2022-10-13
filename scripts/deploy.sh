f1e2aac63c961cc27cf2b22671855b398993155a
hwicode <khc3796@naver.com>
2022-10-13 14:50:43 +0900

    배포용 스크립트에서 CURRENT_PID를 가져오는 명령어 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f springboot2-webservice.*.jar)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
