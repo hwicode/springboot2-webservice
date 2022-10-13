04e59a0b5b88d385aa144ea59c5ddcec86202ef7
hwicode <khc3796@naver.com>
2022-10-13 15:10:47 +0900

    배포용 스크립트에서 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ CURRENT_PID=$(pgrep -f springboot2-webservice.*.jar)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRNET_PID" ]; then
+if [ -z $CURRNET_PID ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
